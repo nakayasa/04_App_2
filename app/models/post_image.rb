bc30a5aadd9eba8a5bcfad0fdfeb1310e748f120
EC2 Default User <ec2-user@ip-172-31-47-241.ap-northeast-1.compute.internal>
2022-07-14 09:59:15 +0000

    [finish]4-23

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,6 +3,11 @@ class PostImage < ApplicationRecord
 
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  validates :shop_name, presence:true
+  validates :image, presence:true
+
 
   def get_image
     unless image.attached?
@@ -11,4 +16,9 @@ class PostImage < ApplicationRecord
     end
       image
   end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
